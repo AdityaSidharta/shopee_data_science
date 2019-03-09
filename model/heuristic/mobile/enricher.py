c72ce115459a995690cc4cbaca866ed86d314cd4
TAN KAI HANG <a0125151@xgpd5.comp.nus.edu.sg>
2019-03-09 17:03:43 +0800

    Able to enrich OS

--- model/heuristic/mobile/enricher.py
+++ model/heuristic/mobile/enricher.py
@@ -10,15 +10,19 @@ class Enricher:
 
     def __init__(self):
         self.Fon = FonApi(env.fono_key)
+        self.phones_from_api = []
 
     def enrich(self, extracted):
-        phones = self.search(extracted)
-        if len(phones) == 0:
-            print("No Matches Found.")
-        elif len(phones) > 1:
-            print("Ahh!")
-        elif len(phones) == 1:
-            print(json.dumps(phones[0], indent=4))
+        self.phones_from_api = self.search(extracted)
+        extracted["Enriched"] = True
+        if len(self.phones_from_api) == 0:
+            extracted["Enriched"] = False
+        elif len(self.phones_from_api) > 1:
+            # print(json.dumps(self.phones_from_api, indent=4))
+            extracted["Operating System"] = self.get_os()
+        elif len(self.phones_from_api) == 1:
+            # print(json.dumps(self.phones_from_api[0], indent=4))
+            extracted["Operating System"] = self.get_os()
         return extracted
 
     def search(self, extracted):
@@ -54,3 +58,37 @@ class Enricher:
                 phones = []
             open(filepath, "w").write(json.dumps(phones))
         return phones
+
+    def get_os(self):
+        phones = self.phones_from_api
+        os_set = set()
+        known_systems = [
+                "ios", "android", "symbian", "samsung os",
+                "blackberry os", "windows", "nokia os"
+                ]
+        excluded_systems = [
+                "tizen-based", "tizen", "watchos"
+                ]
+        for phone in phones:
+            if "os" not in phone:
+                return ""
+            os_str = phone["os"].lower()
+            for os in (known_systems + excluded_systems):
+                if self.string_found(os, os_str):
+                    os_set.add(os)
+        if len(list(os_set)) == 1:
+            os = list(os_set)[0]
+            if os in excluded_systems:
+                return ""
+            else:
+                return os
+        elif len(list(os_set)) > 1:
+            # TODO account for more than one OS
+            return ""
+        else: # 0
+            return ""
+
+    def string_found(self, substr, mainstr):
+        substr = " " + substr.strip() + " "
+        mainstr = " " + mainstr.strip() + " "
+        return substr in mainstr
